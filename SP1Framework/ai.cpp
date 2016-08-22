4b4fd0776d49bfc08a9d3747f0aa2a0a6de87ec4
unknown <1n506@CL36G409.sit.nyp.edu.sg>
2016-08-22 18:05:57 +0800

    RED AI Configuration

--- SP1Framework/ai.cpp
+++ SP1Framework/ai.cpp
@@ -56,45 +56,45 @@ void motiondetect()
 
 
 
-	if (g_enemy.m_cLocation.X > 0 &&
-		(g_enemy.m_cLocation.X - g_nChar.m_cLocation.X) < 0 &&
-		//g_enemy.m_cLocation.Y == g_nChar.m_cLocation.Y &&
-		//(g_enemy.m_cLocation.X - g_nChar.m_cLocation.X) >= -4) &&
-		map[g_enemy.m_cLocation.Y][g_enemy.m_cLocation.X + 1] != (char)219)
-	{
-		g_enemy.m_cLocation.X++;
-		bSomethingHappened = true;
-	}
+	//if (g_enemy.m_cLocation.X > 0 &&
+	//	(g_enemy.m_cLocation.X - g_nChar.m_cLocation.X) < 0 &&
+	//	//g_enemy.m_cLocation.Y == g_nChar.m_cLocation.Y &&
+	//	//(g_enemy.m_cLocation.X - g_nChar.m_cLocation.X) >= -4) &&
+	//	map[g_enemy.m_cLocation.Y][g_enemy.m_cLocation.X + 1] != (char)219)
+	//{
+	//	g_enemy.m_cLocation.X++;
+	//	bSomethingHappened = true;
+	//}
 
-	if (g_enemy.m_cLocation.X > 0 &&
-		g_enemy.m_cLocation.X - g_nChar.m_cLocation.X > 0 &&
-		//g_enemy.m_cLocation.Y == g_nChar.m_cLocation.Y &&
-		//g_enemy.m_cLocation.X - g_nChar.m_cLocation.X <= 4 &&
-		map[g_enemy.m_cLocation.Y][g_enemy.m_cLocation.X - 1] != (char)219)
-	{
-		g_enemy.m_cLocation.X--;
-		bSomethingHappened = true;
-	}
+	//if (g_enemy.m_cLocation.X > 0 &&
+	//	g_enemy.m_cLocation.X - g_nChar.m_cLocation.X > 0 &&
+	//	//g_enemy.m_cLocation.Y == g_nChar.m_cLocation.Y &&
+	//	//g_enemy.m_cLocation.X - g_nChar.m_cLocation.X <= 4 &&
+	//	map[g_enemy.m_cLocation.Y][g_enemy.m_cLocation.X - 1] != (char)219)
+	//{
+	//	g_enemy.m_cLocation.X--;
+	//	bSomethingHappened = true;
+	//}
 
-	if (g_enemy.m_cLocation.Y > 0 &&
-		(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) < 0 &&
-		//g_enemy.m_cLocation.X == g_nChar.m_cLocation.X &&
-		//(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) >= -4) &&
-		map[g_enemy.m_cLocation.Y + 1][g_enemy.m_cLocation.X] != (char)219)
-	{
-		g_enemy.m_cLocation.Y++;
-		bSomethingHappened = true;
-	}
+	//if (g_enemy.m_cLocation.Y > 0 &&
+	//	(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) < 0 &&
+	//	//g_enemy.m_cLocation.X == g_nChar.m_cLocation.X &&
+	//	//(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) >= -4) &&
+	//	map[g_enemy.m_cLocation.Y + 1][g_enemy.m_cLocation.X] != (char)219)
+	//{
+	//	g_enemy.m_cLocation.Y++;
+	//	bSomethingHappened = true;
+	//}
 
-	if (g_enemy.m_cLocation.Y > 0 &&
-		(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) > 0 &&
-		//g_enemy.m_cLocation.X == g_nChar.m_cLocation.X &&
-		//(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) <= 4) &&
-		map[g_enemy.m_cLocation.Y - 1][g_enemy.m_cLocation.X] != (char)219)
-	{
-		g_enemy.m_cLocation.Y--;
-		bSomethingHappened = true;
-	}
+	//if (g_enemy.m_cLocation.Y > 0 &&
+	//	(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) > 0 &&
+	//	//g_enemy.m_cLocation.X == g_nChar.m_cLocation.X &&
+	//	//(g_enemy.m_cLocation.Y - g_nChar.m_cLocation.Y) <= 4) &&
+	//	map[g_enemy.m_cLocation.Y - 1][g_enemy.m_cLocation.X] != (char)219)
+	//{
+	//	g_enemy.m_cLocation.Y--;
+	//	bSomethingHappened = true;
+	//}
 
 	//Second Character
 	if (g_enemy2.m_cLocation.X > 0 &&
--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -120,7 +120,8 @@ void getInput(void)
 	g_abKeyPressed[K_SPACE] = isKeyPressed(VK_SPACE);
 	g_abKeyPressed[K_ESCAPE] = isKeyPressed(VK_ESCAPE);
 	g_abKeyPressed[K_BACK] = isKeyPressed(VK_BACK);
-	g_abKeyPressed[K_SHIFT] = isKeyPressed(VK_LSHIFT);
+	g_abKeyPressed[K_LSHIFT] = isKeyPressed(VK_LSHIFT);
+	g_abKeyPressed[K_RSHIFT] = isKeyPressed(VK_RSHIFT);
 
 	//WASD
 	g_abKeyPressed[K_W] = isKeyPressed(VK_W);
@@ -220,29 +221,61 @@ void moveCharacter()
 
 	// Updating the location of the character based on the key press
 	// providing a beep sound whenver we shift the character
+	if (g_abKeyPressed[K_RSHIFT] && g_abKeyPressed[K_W] &&
+		map[g_sChar.m_cLocation.Y - 1][g_sChar.m_cLocation.X] != (char)219)
+	{
+		g_sChar.m_cLocation.Y--;
+		boost = true;
+	}
+	if (g_abKeyPressed[K_RSHIFT] && g_abKeyPressed[K_A] &&
+		map[g_sChar.m_cLocation.Y ][g_sChar.m_cLocation.X-1] != (char)219)
+	{
+		if (g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1 &&
+			map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X - 1] != (char)219 &&
+			map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X - 1] !=
+			map[g_door1.m_cLocation.Y][g_door1.m_cLocation.X])
+		{
+			g_sChar.m_cLocation.X--;
+			boost = true;
+		}
+	}
+	if (g_abKeyPressed[K_RSHIFT] && g_abKeyPressed[K_S] &&
+		map[g_sChar.m_cLocation.Y +1][g_sChar.m_cLocation.X] != (char)219)
+	{
+		g_sChar.m_cLocation.Y++;
+		boost = true;
+	}
+	if (g_abKeyPressed[K_RSHIFT] && g_abKeyPressed[K_D] &&
+		map[g_sChar.m_cLocation.Y ][g_sChar.m_cLocation.X +1] != (char)219)
+	{
+		g_sChar.m_cLocation.X++;
+		boost = true;
+	}
 
-	if (g_abKeyPressed[K_SHIFT] && g_abKeyPressed[K_UP] &&
+
+
+	if (g_abKeyPressed[K_LSHIFT] && g_abKeyPressed[K_UP] &&
 		map[g_nChar.m_cLocation.Y - 1][g_nChar.m_cLocation.X] != (char)219)
 	{
 		g_nChar.m_cLocation.Y--;
 		boost = true;
 	}
 
-	if (g_abKeyPressed[K_SHIFT] && g_abKeyPressed[K_LEFT] &&
+	if (g_abKeyPressed[K_LSHIFT] && g_abKeyPressed[K_LEFT] &&
 		map[g_nChar.m_cLocation.Y][g_nChar.m_cLocation.X - 1] != (char)219)
 	{
 		g_nChar.m_cLocation.X--;
 		boost = true;
 	}
 
-	if (g_abKeyPressed[K_SHIFT] && g_abKeyPressed[K_DOWN] &&
+	if (g_abKeyPressed[K_LSHIFT] && g_abKeyPressed[K_DOWN] &&
 		map[g_nChar.m_cLocation.Y + 1][g_nChar.m_cLocation.X] != (char)219)
 	{
 		g_nChar.m_cLocation.Y++;
 		boost = true;
 	}
 
-	if (g_abKeyPressed[K_SHIFT] && g_abKeyPressed[K_RIGHT] &&
+	if (g_abKeyPressed[K_LSHIFT] && g_abKeyPressed[K_RIGHT] &&
 		map[g_nChar.m_cLocation.Y][g_nChar.m_cLocation.X + 1] != (char)219)
 	{
 		g_nChar.m_cLocation.X++;
@@ -277,8 +310,7 @@ void moveCharacter()
 			if (door1 == true)
 			{
 				if (g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1 &&
-					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X + 1] != (char)219 &&
-					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X + 1])
+					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X - 1] != (char)219)
 				{
 					g_sChar.m_cLocation.X--;
 					bSomethingHappened = true;
@@ -288,9 +320,10 @@ void moveCharacter()
 			if (door1 == false)
 			{
 				if (g_sChar.m_cLocation.X < g_Console.getConsoleSize().X - 1 &&
-					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X + 1] != (char)219 &&
+					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X - 1] != (char)219 &&
 					map[g_sChar.m_cLocation.Y][g_sChar.m_cLocation.X - 1] !=
-					map[g_door1.m_cLocation.Y][g_door1.m_cLocation.X])
+					map[g_door1.m_cLocation.Y][g_door1.m_cLocation.X] )
+			
 				{
 					g_sChar.m_cLocation.X--;
 					bSomethingHappened = true;
